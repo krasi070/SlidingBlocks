35281d234c9a3e4c8562e8b8b2ba0ef33c3f7b7c
krasi070 <krasi0707@gmail.com>
2019-11-29 15:45:17 +0000

    added PuzzlePiece script

--- Sliding Blocks/Assets/Scripts/Puzzle.cs
+++ Sliding Blocks/Assets/Scripts/Puzzle.cs
@@ -4,7 +4,7 @@ public class Puzzle : MonoBehaviour
 {
     public int size = 4;
 
-    private GameObject _hiddenPiece;
+    private PuzzlePiece _hiddenPiece;
 
     private void Start()
     {
@@ -12,44 +12,40 @@ public class Puzzle : MonoBehaviour
         Camera.main.orthographicSize = size * 0.55f;
     }
 
-    private void Update()
+    private void InitQuads()
     {
-        if (Input.GetMouseButtonDown(0))
+        for (float row = 0; row < size; row++)
         {
-            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
-            RaycastHit hit;
-
-            if (Physics.Raycast(ray, out hit))
+            for (float col = 0; col < size; col++)
             {
-                float distance = Mathf.Abs(Vector3.Distance(_hiddenPiece.transform.position, hit.transform.position));
-                if (Mathf.Approximately(distance, 1f))
+                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
+                quad.transform.position = new Vector3(col - (size / 2f) + 0.5f, row - (size / 2f) + 0.5f, 0f);
+                quad.transform.parent = transform;
+
+                PuzzlePiece puzzlePiece = quad.AddComponent<PuzzlePiece>();
+                puzzlePiece.OnPuzzlePiecePressed += MovePuzzlePiece;
+                puzzlePiece.coordinates = new Vector2(row, col);
+
+                if (row == 0 && col == size - 1)
                 {
-                    Vector3 tempPos = _hiddenPiece.transform.position;
-                    _hiddenPiece.transform.position = hit.transform.position;
-                    hit.transform.position = tempPos;
+                    quad.SetActive(false);
+                    _hiddenPiece = puzzlePiece;
                 }
             }
         }
     }
 
-    private void InitQuads()
+    private void MovePuzzlePiece(PuzzlePiece puzzlePiece)
     {
-        float start = -size / 2f + 0.5f;
-        float end = size / 2f - 0.5f;
-        for (float row = start; row <= end; row++)
+        if ((puzzlePiece.coordinates - _hiddenPiece.coordinates).sqrMagnitude == 1f)
         {
-            for (float col = start; col <= end; col++)
-            {
-                GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
-                quad.transform.position = new Vector3(col, row , 0f);
-                quad.transform.parent = transform;
+            Vector2 tempCoord = _hiddenPiece.coordinates;
+            _hiddenPiece.coordinates = puzzlePiece.coordinates;
+            puzzlePiece.coordinates = tempCoord;
 
-                if (row == start && col == end)
-                {
-                    _hiddenPiece = quad;
-                    _hiddenPiece.SetActive(false);
-                }
-            }
+            Vector3 tempPos = _hiddenPiece.transform.position;
+            _hiddenPiece.transform.position = puzzlePiece.transform.position;
+            puzzlePiece.transform.position = tempPos;
         }
     }
 }
---
+++ Sliding Blocks/Assets/Scripts/PuzzlePiece.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using UnityEngine;
+
+public class PuzzlePiece : MonoBehaviour
+{
+    public event Action<PuzzlePiece> OnPuzzlePiecePressed;
+
+    public Vector2 coordinates;
+
+    private void OnMouseDown()
+    {
+        OnPuzzlePiecePressed?.Invoke(this);
+    }
+}
