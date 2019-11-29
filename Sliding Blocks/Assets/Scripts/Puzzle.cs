ff3725d1c28c31d786e35b952ef46a553891b60d
krasi070 <krasi0707@gmail.com>
2019-11-29 15:06:40 +0000

    implemented input detection and puzzle piece sliding

--- Sliding Blocks/Assets/Scripts/Puzzle.cs
+++ Sliding Blocks/Assets/Scripts/Puzzle.cs
@@ -4,10 +4,32 @@ public class Puzzle : MonoBehaviour
 {
     public int size = 4;
 
+    private GameObject _hiddenPiece;
+
     private void Start()
     {
         InitQuads();
         Camera.main.orthographicSize = size * 0.55f;
+    }
+
+    private void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit hit;
+
+            if (Physics.Raycast(ray, out hit))
+            {
+                float distance = Mathf.Abs(Vector3.Distance(_hiddenPiece.transform.position, hit.transform.position));
+                if (Mathf.Approximately(distance, 1f))
+                {
+                    Vector3 tempPos = _hiddenPiece.transform.position;
+                    _hiddenPiece.transform.position = hit.transform.position;
+                    hit.transform.position = tempPos;
+                }
+            }
+        }
     }
 
     private void InitQuads()
@@ -21,6 +43,12 @@ public class Puzzle : MonoBehaviour
                 GameObject quad = GameObject.CreatePrimitive(PrimitiveType.Quad);
                 quad.transform.position = new Vector3(col, row , 0f);
                 quad.transform.parent = transform;
+
+                if (row == start && col == end)
+                {
+                    _hiddenPiece = quad;
+                    _hiddenPiece.SetActive(false);
+                }
             }
         }
     }
